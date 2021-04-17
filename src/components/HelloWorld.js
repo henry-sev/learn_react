97578d09302d01b93031b8fc3929e723595d8769
henry-sev <q2466351926@gmail.com>
2021-04-17 09:41:06 +0800

    first page

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./HelloWorld.css";
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="font-color">第一个react页面</h1>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+
+export default HelloWorld;
